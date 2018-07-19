670a7a14da2c74feaf276c51a67dab708c0101fa
長谷川良 <hase.industries@i.softbank.jp>
2018-07-20 00:37:48 +0900

    再生ボタンを使用した後、停止するためのボタンに切り替えられない タップ数をカウントして １タップ目２タップ目の挙動を作る次回はカウント方法と初期化を作る

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,11 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var images: UIImageView!
     //再生ボタンの準備
     @IBOutlet weak var playbutton: UIButton!
+    //スライドショーのステータスを判別してボタンの表示を再生にするメソッド
+    func slidestatus(){
+       playbutton.setTitle("再生", for: .normal)}
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        
+        slidestatus()
         
     }
     //まずは画像の配列を作る
@@ -26,16 +31,27 @@ class ViewController: UIViewController {
                UIImage(named: "IMG3")].compactMap{$0}
     
     //再生アクション
+    //タップのカウント用変数
+    var count = 0
     @IBAction func play(_ sender: Any) {
-        //ボタンの切り替え
-        
+        //1タップ目の挙動→スタート、ラベルを”停止”に
+        //２タップ目の挙動→ストップ、ラベルを”再生”に変数を初期化
         //スライドの設定
         images.animationImages = IMG
         images.animationDuration = 6.0
-        
         //開始
         images.startAnimating()
+        //停止
+        images.stopAnimating()
+        //ボタンの切り替え
+        playbutton.setTitle("停止", for: .normal)
+        playbutton.setTitle("再生", for: .normal)
+        
+        
     }
+    //停止アクション
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
