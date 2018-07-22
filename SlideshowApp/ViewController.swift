161c385eaa101cd140bd0600b64a8721f2b049e1
長谷川良 <hase.industries@i.softbank.jp>
2018-07-23 00:31:21 +0900

    再生ボタンを押した瞬間に画像が切り替わる理由がわからない ２枚目以降はおけ 停止ボタンを押したら表示中の画像で止まるようにしたい。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,48 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //表示場所の準備
-    @IBOutlet weak var images: UIImageView!
+    
     //再生ボタンの準備
     @IBOutlet weak var playbutton: UIButton!
-    //スライドショーのステータスを判別してボタンの表示を再生にするメソッド
-    func slidestatus(){
-       playbutton.setTitle("再生", for: .normal)}
-    
+    //進むボタンの準備
+    @IBOutlet weak var goNext: UIButton!
+    //戻るボタンの準備
+    @IBOutlet weak var goBack: UIButton!
+    //表示場所の準備
+    @IBOutlet weak var images: UIImageView!
+    //表示する画像の格納庫
+    let imageindex = [#imageLiteral(resourceName: "IMG1"),#imageLiteral(resourceName: "IMG2"),#imageLiteral(resourceName: "IMG3")]
+    //表示する画像の配列番号
+    var dispimageno = 0
+    //画像を配列から取得して表示するメソッド
+    func dispimage() {
+        let name = imageindex[dispimageno]
+        images.image = name
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        slidestatus()
-        
-    }
-    //まずは画像の配列を作る
-    var IMG = [UIImage(named: "IMG1"),
-               UIImage(named: "IMG2"),
-               UIImage(named: "IMG3")].compactMap{$0}
-    
-    //再生アクション
-    //タップのカウント用変数
-    var count = 0
-    @IBAction func play(_ sender: Any) {
-        //1タップ目の挙動→スタート、ラベルを”停止”に
-        //２タップ目の挙動→ストップ、ラベルを”再生”に変数を初期化
-        //スライドの設定
-        images.animationImages = IMG
-        images.animationDuration = 6.0
-        //開始
-        images.startAnimating()
-        //停止
-        images.stopAnimating()
-        //ボタンの切り替え
-        playbutton.setTitle("停止", for: .normal)
+        //最初の画像を表示しておく
+        images.image = imageindex[0]
         playbutton.setTitle("再生", for: .normal)
         
-        
     }
-    //停止アクション
     
+    //再生アクション(実装途中)
+    //
+    var timer: Timer!
+    
+    
+    @objc func updatetimer(){
+        
+        if dispimageno < imageindex.count-1{
+            dispimageno += 1
+            dispimage()
+        }
+        else{dispimageno = 0
+            dispimage()
+        }
+    }
+    @IBAction func play(_ sender: Any) {
+        
+        
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updatetimer), userInfo: nil, repeats: true)
+            playbutton.setTitle("停止", for: .normal)
+            goBack.isEnabled = false
+            goNext.isEnabled = false
+            updatetimer()
+        }
+            
+        else{
+            self.timer.invalidate()
+            timer = nil
+            playbutton.setTitle("再生", for: .normal)
+            goBack.isEnabled = true
+            goNext.isEnabled = true
+        }
+    }
+    //進むアクション
+    @IBAction func goNext(_ sender: Any) {
+        
+        if dispimageno < imageindex.count-1{
+            dispimageno += 1
+            dispimage()
+        }
+        else{dispimageno = 0
+            dispimage()
+        }
+    }
+    //戻るアクション
+    @IBAction func goBack(_ sender: Any) {
+        if dispimageno > 0{
+            dispimageno -= 1
+            dispimage()
+        }
+        else{dispimageno = imageindex.count-1
+            dispimage()}
+    }
+    //進む戻るはそれぞれ逆の処理をさせる。
     
     
     override func didReceiveMemoryWarning() {
